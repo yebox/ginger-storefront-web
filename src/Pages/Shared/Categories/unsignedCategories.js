fc5ddf30e6434235815fe90df4a4fe36d8340932
Victor Ama <amavictor47@gmail.com>
2024-03-20 15:13:18 +0100

    mesrp remove

--- src/Pages/Shared/Categories/unsignedCategories.js
+++ src/Pages/Shared/Categories/unsignedCategories.js
@@ -112,7 +112,7 @@ const UnsignedCategories = () => {
             <h4>Become a seller on Ginger</h4>
             <p>Browse more top selling products from top brands</p>
             <div>
-              <GButton label="Sign up now" onClick={() => navigate("/login")} />
+              {/* <GButton label="Sign up now" onClick={() => navigate("/login")} /> */}
               <GButton
                 outline
                 onClick={() => navigate("/sell-on-ginger")}
